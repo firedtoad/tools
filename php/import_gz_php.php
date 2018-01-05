d90335faac067cbc4e8bd8a0c06bb9225a5d0311
dietoad <dietoad@gmail.com>
2018-01-05 14:29:19 +0800

    import for cluster crc16

--- php/import_gz_php.php
+++ php/import_gz_php.php
@@ -1,10 +1,13 @@
 <?php
 ini_set('memory_limit', '1024M');
+define('SLOTS',16384);
+require 'crc16.php';
 function import_json($redis,$data) {
     $redis->multi(Redis::PIPELINE);
     foreach ($data as $dk=>$dv)
     {
         $key=$dv['key'];
+
         $type=$dv['type'];
         $value=$dv['value'];
         if(is_array($value)&&count($value))
@@ -22,6 +25,7 @@ function import_json($redis,$data) {
                 }
             }
         }
+        
         if ($type == Redis::REDIS_STRING) {
              $redis->set($key,$value);
         }
@@ -45,9 +49,11 @@ function import_json($redis,$data) {
         }
      }
      $ret=$redis->exec();
+    $err=$redis->getlasterror();
+    print_r($err);
 }
-$ip='172.18.40.3';
-// $ip='172.18.10.1';
+// $ip='172.18.40.3';
+$ip='172.18.10.1';
 $st_port=7001;
 $ed_port=7006;
 if(isset($argv[1]))
@@ -65,31 +71,55 @@ foreach ($ports as $pk=>$pv)
 }
 $redis = new RedisCluster(NULL, $hosts);
 $masters=$redis->_masters();
+$slots=$redis->cluster($masters[0],'slots');
 // $rank=$cluster->zrange('z-2:rank:14',0,-1,1);
 // print_r($rank);
-$import=file_get_contents('172.18.10.1.json');
+$import=file_get_contents('127.0.0.1gz_.json');
 $importData=json_decode($import,true);
 foreach ($hosts as $hk=>$hv)
 {
     $redis->flushAll($hv);
 }
-$count=10000;
+$count=1000;
 $index=0;
-// exit();
+$rslots=[];
+
+foreach ($slots as $k=>$v)
+{
+    $lslots=[$v[0],$v[1],$v[2][0],$v[2][1]];
+    $rslots[]=$lslots;
+}
+// print_r($rslots);
+// print_r($redis);
+// ReflectionClass::export($redis);
+
+$servers=[];
+foreach ($rslots as $k=>$v)
+{
+    $servers[$v[2].':'.$v[3]]=[$v[2],$v[3],[]];
+}
+
 foreach ($importData as $k=>$v)
 {
-    $config=$masters[$index++];
-    $r=new Redis();
-    $r->connect($config[0],$config[1]);
     foreach ($v as $kv=>&$vv)
-    {
+    {   
         $vv['key']=$kv;
+        $slot=crc16($kv)%SLOTS;
+        foreach ($rslots as $ks=>$vs)
+        {
+            if($vs[0]<=$slot&&$vs[1]>=$slot)
+            {
+                $servers[$vs[2].':'.$vs[3]][2][]=$vv;
+                break;
+            }
+        }
     }
-    $skeys=array_chunk($v, $count);
-    foreach ($skeys as $key=>$value)
-    {
-        import_json($r,$value);
-    }
+}
+foreach ($servers as $k=>$v)
+{
+    $r=new Redis();
+    $r->connect($v[0],$v[1]);
+    import_json($r, $v[2]);
 }
 
 
